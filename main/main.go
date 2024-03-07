83c5722e833f0e15b2cece7a2b370a022c685ed5
Dont Serious <1090087271@qq.com>
2024-03-07 20:09:33 +0800

    day5 Add HTTP support Debug interface

--- main/main.go
+++ main/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"geerpc"
 	"log"
 	"net"
+	"net/http"
+	"sync"
+	"time"
 )
 
 type Foo int
@@ -15,21 +19,61 @@ func (f Foo) Sum(args Args, reply *int) error {
 	return nil
 }
 
-func startServer(addr chan string) {
+// day5
+func startServer(addrCh chan string) {
 	var foo Foo
-	if err := geerpc.Register(&foo); err != nil {
-		log.Fatal("register error: ", err)
-	}
-
-	// pick a free port
-	l, err := net.Listen("tcp", ":0")
-	if err != nil {
-		log.Fatal("network error:", err)
-	}
-	log.Println("start rpc server on", l.Addr())
-	addr <- l.Addr().String()
-	geerpc.Accept(l)
+	l, _ := net.Listen("tcp", ":9999")
+	_ = geerpc.Register(&foo)
+	geerpc.HandleHTTP()
+	addrCh <- l.Addr().String()
+	_ = http.Serve(l, nil)
 }
+
+func call(addrCh chan string) {
+	client, _ := geerpc.DialHTTP("tcp", <-addrCh)
+	defer func() { _ = client.Close() }()
+
+	time.Sleep(time.Second)
+	// send request & receive response
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
+			if err := client.Call(context.Background(), "Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
+		}(i)
+	}
+	wg.Wait()
+}
+
+func main() {
+	log.SetFlags(0)
+	ch := make(chan string)
+	go call(ch)
+	startServer(ch)
+}
+
+//
+// func startServer(addr chan string) {
+// 	var foo Foo
+// 	if err := geerpc.Register(&foo); err != nil {
+// 		log.Fatal("register error: ", err)
+// 	}
+
+// 	// pick a free port
+// 	l, err := net.Listen("tcp", ":0")
+// 	if err != nil {
+// 		log.Fatal("network error:", err)
+// 	}
+// 	log.Println("start rpc server on", l.Addr())
+// 	addr <- l.Addr().String()
+// 	geerpc.Accept(l)
+// }
 
 // func main() {
 // 	log.SetFlags(0)
